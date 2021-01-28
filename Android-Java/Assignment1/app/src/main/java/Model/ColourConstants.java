5b91b5efbde2798360c8c3c76b025b1fd6861afb
Cliveswe <clive.leddy@gmail.com>
2021-01-28 19:48:27 +0100

    Removed project.
    Removed all android projects.

--- Android-Java/Assignment1/app/src/main/java/Model/ColourConstants.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-/**
- * Created by Clive Leddy on 2018-02-21.
- */
-
-public enum ColourConstants {WHITE, RED,GREY}
--- Android-Java/Assignment1/app/src/main/java/Model/ParcelConveter.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-/**
- * Created by Clive Leddy on 2018-02-20.
- */
-
-interface ParcelConveter<T> {
-}
